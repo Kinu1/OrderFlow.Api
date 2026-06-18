4d985bd8b0c1509e6e776edf1e412b7dc11e600a
Pedro Loubach <pedroloubach10@gmail.com>
2026-06-18 16:52:10 -0300

    Adiciona crud inicial de clientes em memoria

--- src/OrderFlow.Api/Program.cs
+++ src/OrderFlow.Api/Program.cs
@@ -1,10 +1,13 @@
+using OrderFlow.Api.Application.Interfaces;
 using OrderFlow.Api.Application.Services;
+using OrderFlow.Api.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddScoped<ClienteService>();
+builder.Services.AddSingleton<IClienteRepository, ClienteRepository>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,4 +27,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
