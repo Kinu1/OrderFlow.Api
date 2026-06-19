294fce5e47dcfb586eb2e3e1ca9022bd566d54c8
Pedro Loubach <pedroloubach10@gmail.com>
2026-06-19 17:14:08 -0300

    Conexão com banca de dados SQL Pronta

--- src/OrderFlow.Api/Infrastructure/Repositories/ClienteRepository.cs
+++ src/OrderFlow.Api/Infrastructure/Repositories/ClienteRepository.cs
@@ -1,58 +1,132 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
 using OrderFlow.Api.Application.Interfaces;
 using OrderFlow.Api.Domain.Entities;
+using OrderFlow.Api.Infrastructure.Data;
 
 namespace OrderFlow.Api.Infrastructure.Repositories;
 
 public class ClienteRepository : IClienteRepository
 {
-    private static readonly List<Cliente> Clientes = [];
-    private static int _proximoId = 1;
+    private readonly SqlConnectionFactory _connectionFactory;
 
-    public Task<int> CriarAsync(Cliente cliente)
+    public ClienteRepository(SqlConnectionFactory connectionFactory)
     {
-        cliente.Id = _proximoId++;
-        cliente.DateTime = DateTime.Now;
-
-        Clientes.Add(cliente);
-
-        return Task.FromResult(cliente.Id);
+        _connectionFactory = connectionFactory;
     }
 
-    public Task<List<Cliente>> ListarAsync()
+    public async Task<int> CriarAsync(Cliente cliente)
     {
-        return Task.FromResult(Clientes.ToList());
+        await using var conexao = _connectionFactory.CreateConnection();
+
+        await using var comando = new SqlCommand("Cliente_Criar", conexao);
+        comando.CommandType = CommandType.StoredProcedure;
+
+        comando.Parameters.AddWithValue("@Nome", cliente.Nome);
+        comando.Parameters.AddWithValue("@Email", cliente.Email);
+        comando.Parameters.AddWithValue("@Telefone", cliente.Telefone);
+
+        await conexao.OpenAsync();
+
+        var resultado = await comando.ExecuteScalarAsync();
+
+        return Convert.ToInt32(resultado);
     }
 
-    public Task<Cliente?> ObterPorIdAsync(int id)
+    public async Task<List<Cliente>> ListarAsync()
     {
-        var cliente = Clientes.FirstOrDefault(cliente => cliente.Id == id);
+        var clientes = new List<Cliente>();
 
-        return Task.FromResult(cliente);
+        await using var conexao = _connectionFactory.CreateConnection();
+
+        await using var comando = new SqlCommand("Cliente_Listar", conexao);
+        comando.CommandType = CommandType.StoredProcedure;
+
+        await conexao.OpenAsync();
+
+        await using var reader = await comando.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+            clientes.Add(new Cliente
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                Nome = reader.GetString(reader.GetOrdinal("Nome")),
+                Email = reader.GetString(reader.GetOrdinal("Email")),
+                Telefone = reader.IsDBNull(reader.GetOrdinal("Telefone"))
+                    ? string.Empty
+                    : reader.GetString(reader.GetOrdinal("Telefone")),
+                DateTime = reader.GetDateTime(reader.GetOrdinal("CriadoEm"))
+            });
+        }
+
+        return clientes;
     }
 
-    public Task<bool> AtualizarAsync(Cliente cliente)
+    public async Task<Cliente?> ObterPorIdAsync(int id)
     {
-        var clienteExistente = Clientes.FirstOrDefault(c => c.Id == cliente.Id);
+        await using var conexao = _connectionFactory.CreateConnection();
 
-        if (clienteExistente is null)
-            return Task.FromResult(false);
+        await using var comando = new SqlCommand("Cliente_ObterPorId", conexao);
+        comando.CommandType = CommandType.StoredProcedure;
 
-        clienteExistente.Nome = cliente.Nome;
-        clienteExistente.Email = cliente.Email;
-        clienteExistente.Telefone = cliente.Telefone;
+        comando.Parameters.AddWithValue("@Id", id);
 
-        return Task.FromResult(true);
+        await conexao.OpenAsync();
+
+        await using var reader = await comando.ExecuteReaderAsync();
+
+        if (!await reader.ReadAsync())
+            return null;
+
+        return new Cliente
+        {
+            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+            Nome = reader.GetString(reader.GetOrdinal("Nome")),
+            Email = reader.GetString(reader.GetOrdinal("Email")),
+            Telefone = reader.IsDBNull(reader.GetOrdinal("Telefone"))
+                ? string.Empty
+                : reader.GetString(reader.GetOrdinal("Telefone")),
+            DateTime = reader.GetDateTime(reader.GetOrdinal("CriadoEm"))
+        };
     }
 
-    public Task<bool> ExcluirAsync(int id)
+    public async Task<bool> AtualizarAsync(Cliente cliente)
     {
-        var cliente = Clientes.FirstOrDefault(cliente => cliente.Id == id);
+        await using var conexao = _connectionFactory.CreateConnection();
 
-        if (cliente is null)
-            return Task.FromResult(false);
+        await using var comando = new SqlCommand("Cliente_Atualizar", conexao);
+        comando.CommandType = CommandType.StoredProcedure;
 
-        Clientes.Remove(cliente);
+        comando.Parameters.AddWithValue("@Id", cliente.Id);
+        comando.Parameters.AddWithValue("@Nome", cliente.Nome);
+        comando.Parameters.AddWithValue("@Email", cliente.Email);
+        comando.Parameters.AddWithValue("@Telefone", cliente.Telefone);
 
-        return Task.FromResult(true);
+        await conexao.OpenAsync();
+
+        var resultado = await comando.ExecuteScalarAsync();
+
+        var linhasAfetadas = Convert.ToInt32(resultado);
+
+        return linhasAfetadas > 0;
+    }
+
+    public async Task<bool> ExcluirAsync(int id)
+    {
+        await using var conexao = _connectionFactory.CreateConnection();
+
+        await using var comando = new SqlCommand("Cliente_Excluir", conexao);
+        comando.CommandType = CommandType.StoredProcedure;
+
+        comando.Parameters.AddWithValue("@Id", id);
+
+        await conexao.OpenAsync();
+
+        var resultado = await comando.ExecuteScalarAsync();
+
+        var linhasAfetadas = Convert.ToInt32(resultado);
+
+        return linhasAfetadas > 0;
     }
 }
--- src/OrderFlow.Api/Program.cs
+++ src/OrderFlow.Api/Program.cs
@@ -1,13 +1,16 @@
 using OrderFlow.Api.Application.Interfaces;
 using OrderFlow.Api.Application.Services;
+using OrderFlow.Api.Infrastructure.Data;
 using OrderFlow.Api.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddSingleton<SqlConnectionFactory>();
+
 builder.Services.AddScoped<ClienteService>();
-builder.Services.AddSingleton<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
